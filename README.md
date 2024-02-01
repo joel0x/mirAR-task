c7b92d34a38bb4e962e70fa5919fdded702b5f4d
Joel Machado <77497620+joel0x@users.noreply.github.com>
2024-02-01 20:12:15 +0530

    upload files

---
+++ README.md
@@ -0,0 +1,6 @@
+#npm install
+
+#npm run dev
+
+#add your api key om config.js
+
---
+++ src/config/config.js
@@ -0,0 +1,2 @@
+export const hostName = "https://api.openweathermap.org";
+export const appId = "0b4b3340040e20533aa685e632cf9fba";
